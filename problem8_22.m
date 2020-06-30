13a39db1ce422d24c5039790da72c7fb17052846
Daniel_Zhang <shengjun.zhang@nyu.edu>
2020-06-29 20:39:27 -0500

    Update problem8_22.m

--- problem8_22.m
+++ problem8_22.m
@@ -11,22 +11,22 @@
 clc; clear; close all;
 %-------------------------------------------------------------------------%
 % % Initialize different parameters
-    A = 10;
-    N = 200;
-    A_estimator = zeros(1, N);
-    K = zeros(1, N);
-    var_A = zeros(1, N);
-    sigma2_0 = 1;
-    x = zeros(1, N);
+A = 10;
+N = 200;
+A_estimator = zeros(1, N);
+K = zeros(1, N);
+var_A = zeros(1, N);
+sigma2_0 = 1;
+x = zeros(1, N);
 %-------------------------------------------------------------------------%
 % % For different r 
 r_list = [1, 0.95, 1.05];
 %-------------------------------------------------------------------------%
-    x0 = A + sqrt(sigma2_0)*randn;
-    A_estimator(1) = x0;
-    var_A(1) = sigma2_0;
-    x(1) = x0;
-    K(1) = 0.5; % % From (8.41)
+x0 = A + sqrt(sigma2_0)*randn;
+A_estimator(1) = x0;
+var_A(1) = sigma2_0;
+x(1) = x0;
+K(1) = 0.5; % % From (8.41)
 %-------------------------------------------------------------------------%
 % % Monte Carlo Simulation, generate data and update estimator, gain and variance.
 
@@ -39,6 +39,7 @@ for j = 1:length(r_list)
             A_estimator(i) = A_estimator(i-1)+K(i)*(x(i)-A_estimator(i-1));       
             var_A(i) = (1-K(i))*var_A(i-1);
     end
+    
     figure;
     iteration = 1 : N;
 
@@ -58,7 +59,6 @@ for j = 1:length(r_list)
     plot(iteration,  var_A);
     xlabel('Current sample, N'),ylabel('Variance');
     sgtitle(['r = ', num2str(r)], 'Interpreter', 'LaTeX');
-  
     
 end
 %-------------------------------------------------------------------------%
